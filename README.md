8d218bc55fb203ff049a5af0e51b79a541041c39
Michael <nuritdeveloper@gmail.com>
2021-10-09 07:16:45 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AppleBumpClient
+
+A description of this package.
---
+++ Sources/AppleBumpClient/AppleBumpClient.swift
@@ -0,0 +1,6 @@
+public struct AppleBumpClient {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
